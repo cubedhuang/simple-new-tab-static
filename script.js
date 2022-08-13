fbde827273a328e9c920a681a17ab9d108c9d1e7
cubedhuang <99danielh@gmail.com>
2022-08-13 15:16:18 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+const { tw } = twind;
+
+// ----- Quote -----
+
+const quote = document.getElementById("quote");
+const quoteAuthor = document.getElementById("quote-author");
+
+fetch("https://api.quotable.io/random?tags=future|technology|science")
+	.then(res => res.json())
+	.then(data => {
+		quote.textContent = `"${data.content}"`;
+		quoteAuthor.textContent = data.author;
+	});
+
+// ----- Clock -----
+
+const clock = document.getElementById("clock");
+
+const df = new Intl.DateTimeFormat("en", {
+	month: "long",
+	day: "numeric",
+	year: "numeric",
+	weekday: "long"
+});
+const tf = new Intl.DateTimeFormat("en", {
+	hour: "numeric",
+	minute: "numeric",
+	second: "numeric"
+});
+
+function setClock() {
+	const date = new Date();
+	clock.innerText = `${df.format(date)}\n${tf.format(date)}`;
+}
+
+setInterval(() => setClock(), 1000);
+setClock();
+
+// ----- Presence -----
+
+const ul = document.getElementById("presence");
+
+lanyard({
+	userId: "299707523370319883",
+	socket: true,
+	onPresenceUpdate(presence) {
+		ul.innerHTML = presence.activities
+			.map(({ name, state, details }) => {
+				return `<li>
+					<span class="${tw`text-white`}">${name}</span>
+					${state ? `<br>${state}` : ""}
+					${details ? `<br>${details}` : ""}
+				</li>`;
+			})
+			.join("");
+	}
+});
